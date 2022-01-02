aefdd91e244f61a9530a8cf02ffbe71e2575b724
vishal78689 <vishalthakur78689@gmail.com>
2022-01-02 17:27:07 +0530

    hooks

---
+++ hooks/effect-hook/src/components/HookCounterOne.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+
+function HookCounterOne() {
+	const [count, setCount] = useState(0)
+	const [name, setName] = useState('')
+	useEffect(() => {
+		console.log('useEffect - Updating document title ')
+		document.title = `You clicked ${count} times`
+	}, [count])//to make it run only count changes
+	//runs with every render
+	return (
+		<div>
+			<input type="text" value={name} onChange={e => setName(e.target.value)} />
+			<button onClick={() => setCount(count + 1)}>
+				useEffect - Click {count} times
+			</button>
+		</div>
+	)
+}
+
+export default HookCounterOne
